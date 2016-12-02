0ab914a3bd255af826e434ec7e48e19f6bd07eea
joellegg <joellegg@bellsouth.net>
2016-12-02 14:43:59 -0600

    added songs and removed odd characters using js

---
+++ songs.js
@@ -0,0 +1,22 @@
+var songs = [];
+
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+// Each student must add one song to the beginning and the end of the array.
+songs.unshift("Stay Alive > by Jose Gonzalez on the album The Secret Life of Walter Mitty");
+songs.push("All My Days > by Alex Murdoch on the album Time Without Consequence");
+
+// Loop over the array and remove any words or characters that obviously don't belong.
+// Students must find and replace the > character in each item with a - character
+for (var i = 0; i < songs.length; i++) {
+    songs[i] = songs[i].replace(/\*|\@|\!/gi, '');
+    songs[i] = songs[i].replace('>', '-')
+}
+
+// Must add each string to the DOM in index.html in the main content area
+
+console.log(songs);
