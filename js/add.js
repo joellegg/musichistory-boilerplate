cd3029185affb5a194a52ad4eac430d9c8fd3365
joellegg <joellegg@bellsouth.net>
2016-12-23 15:57:59 -0600

    completed styling for the add music section

--- js/add.js
+++ js/add.js
@@ -2,7 +2,6 @@ var addLink = document.getElementById("link-add");
 var addView = document.getElementById("add-view");
 
 addLink.addEventListener("click", function() {
-  homeView.classList.add("hidden");
   listView.classList.add("hidden");
 
   addView.classList.add("visible");
--- js/list.js
+++ js/list.js
@@ -1,9 +1,8 @@
-var listLink = document.getElementById("link-list");
-var listView = document.getElementById("list-view");
+var listLink = document.querySelector("#link-list");
+var listView = document.querySelector("#list-view");
 
 listLink.addEventListener("click", function(event) {
   event.preventDefault();
-  homeView.classList.add("hidden");
   addView.classList.add("hidden");
 
   listView.classList.add("visible");
