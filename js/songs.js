6b578131fa4c5713cf9252e8344f867aadbd3ba2
joellegg <joellegg@bellsouth.net>
2016-12-24 13:17:09 -0600

    added to readMe file for part 4

--- js/songs.js
+++ js/songs.js
@@ -1,10 +1,4 @@
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-
 // Each student must add one song to the beginning and the end of the array.
 songs.unshift("Stay Alive > by Jose Gonzalez on the album The Secret Life of Walter Mitty");
 songs.push("All My Days > by Alex Murdoch on the album Time Without Consequence");
