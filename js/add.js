38ffa2cedc1b0ba6b378a4b610e2ee60ba6838c3
joellegg <joellegg@bellsouth.net>
2017-01-27 14:42:50 -0600

    changed range bar to a search bar

--- js/add.js
+++ js/add.js
@@ -25,31 +25,31 @@ let addSongButton = $('#addSong');
 
 // Change sections
 addLink.click(function() {
-    //console.log('this was clicked')
-    listView.addClass("hidden");
-    addView.addClass("visible");
-    addView.removeClass("hidden");
+	//console.log('this was clicked')
+	listView.addClass("hidden");
+	addView.addClass("visible");
+	addView.removeClass("hidden");
 });
 
 // Get inputs when the button is pressed
 addSongButton.click(function() {
-    addSong = $('#songName').val();
-    addArtist = $('#artistName').val();
-    addAlbum = $('#albumName').val();
-    addGenre = $('#genreName').val();
-    // if any fields are blank display alert
-    // else add songs to the array of songs
-    if (addSong === "" || addArtist === "" || addAlbum === "" || addGenre === "") {
-        alert('You must enter all fields');
-    } else {
-        userAddedMusic = {
-            "Song": addSong,
-            "Artist": addArtist,
-            "Album": addAlbum,
-            "Genre": addGenre}
-    };
-    // write variables to the DOM
-    addUserMusic();
+	addSong = $('#songName').val();
+	addArtist = $('#artistName').val();
+	addAlbum = $('#albumName').val();
+	addGenre = $('#genreName').val();
+	// if any fields are blank display alert
+	// else add songs to the array of songs
+	if (addSong === "" || addArtist === "" || addAlbum === "" || addGenre === "") {
+		alert('You must enter all fields');
+	} else {
+		userAddedMusic = {
+			"Song": addSong,
+			"Artist": addArtist,
+			"Album": addAlbum,
+			"Genre": addGenre}
+	};
+	// write variables to the DOM
+	addUserMusic();
 })
 
 /////////////////////////////
@@ -59,41 +59,41 @@ addSongButton.click(function() {
 let newHTML = '';
 
 function original5ToHTML() {
-    for (var i = 0; i < original5Music.music.length; i++) {
-        newHTML +=
-            `
-            <div>
-                <h2 class='dark-yellow'>${original5Music.music[i].Song}</h2>
-                <div class='musicRowButton'>
-                    <h4 class='topMargin'>${original5Music.music[i].Artist} | ${original5Music.music[i].Album} | ${original5Music.music[i].Genre}</h4>
-                    <button class='deleteRowButton'>Delete</button>
-                </div>
-            </div>
-            `;
-    }
-    songPanel.html(newHTML)
-    getDeleteRowButtons();
+	for (var i = 0; i < original5Music.music.length; i++) {
+		newHTML +=
+			`
+			<div>
+				<h2 class='dark-yellow'>${original5Music.music[i].Song}</h2>
+				<div class='musicRowButton'>
+					<h4 class='topMargin'>${original5Music.music[i].Artist} | ${original5Music.music[i].Album} | ${original5Music.music[i].Genre}</h4>
+					<button class='deleteRowButton'>Delete</button>
+				</div>
+			</div>
+			`;
+	}
+	songPanel.html(newHTML)
+	getDeleteRowButtons();
 
 }
 
 //load add2Music when the more button is pressed
 function add2MusicHTML() {
-    console.log('music button clicked')
-    let addMusicHTML = '';
-    for (var i = 0; i < add2Music.music.length; i++) {
-        addMusicHTML +=
-            `
-            <div>
-                <h2 class='dark-yellow'>${add2Music.music[i].Song}</h2>
-                <div class='musicRowButton'>
-                    <h4 class='topMargin'>${add2Music.music[i].Artist} | ${add2Music.music[i].Album} | ${add2Music.music[i].Genre}</h4>
-                    <button class='deleteRowButton'>Delete</button>
-                </div>
-            </div>
-            `;
-    }
-    songPanel.append(addMusicHTML)
-    getDeleteRowButtons();
+	console.log('music button clicked')
+	let addMusicHTML = '';
+	for (var i = 0; i < add2Music.music.length; i++) {
+		addMusicHTML +=
+			`
+			<div>
+				<h2 class='dark-yellow'>${add2Music.music[i].Song}</h2>
+				<div class='musicRowButton'>
+					<h4 class='topMargin'>${add2Music.music[i].Artist} | ${add2Music.music[i].Album} | ${add2Music.music[i].Genre}</h4>
+					<button class='deleteRowButton'>Delete</button>
+				</div>
+			</div>
+			`;
+	}
+	songPanel.append(addMusicHTML)
+	getDeleteRowButtons();
 }
 
 
@@ -102,10 +102,10 @@ function add2MusicHTML() {
 /////////////////////////////
 
 $.getJSON('JSON/music.json', function(data) {
-    original5Music = data;
-    original5ToHTML();
+	original5Music = data;
+	original5ToHTML();
 });
 
 $.getJSON('JSON/music2.json', function(data) {
-    add2Music = data;
+	add2Music = data;
 });
